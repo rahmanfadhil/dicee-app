4d5e022ed02bdcab2d65d2cd12c17b7d8f712ea5
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-07-07 13:18:21 +0700

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Rahman Fadhil on 07/07/19.
+//  Copyright © 2019 Rahman Fadhil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
